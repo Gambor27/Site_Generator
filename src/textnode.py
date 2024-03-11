e72d0c8c7a0b904c1c8ff92a667cb7e312f60294
Gambor27 <gambor27@gmail.com>
2024-03-11 15:09:33 -0700

    Adding HTML conversion

--- src/textnode.py
+++ src/textnode.py
@@ -25,31 +25,16 @@ class TextNode:
     
 def text_node_to_html_node(text_node):
     if text_node.text_type == text_type_text:
-        new_leaf = LeafNode(None, text_node.text)
+        return LeafNode(None, text_node.text)
     elif text_node.text_type == text_type_bold:
-        new_leaf = LeafNode("b", text_node.text)
+        return LeafNode("b", text_node.text)
     elif text_node.text_type == text_type_italic:
-        new_leaf = LeafNode("i", text_node.text)
+        return LeafNode("i", text_node.text)
     elif text_node.text_type == text_type_code:
-        new_leaf = LeafNode("code", text_node.text)
+        return LeafNode("code", text_node.text)
     elif text_node.text_type == text_type_link:
-        new_leaf = LeafNode("a", text_node.text, {"href": text_node.url})
+        return LeafNode("a", text_node.text, {"href": text_node.url})
     elif text_node.text_type == text_type_image:
-        new_leaf = LeafNode("image", "", {"src": text_node.url, "alt": text_node.text})
+        return LeafNode("image", "", {"src": text_node.url, "alt": text_node.text})
     else:
         raise Exception("text type not known")
-
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    if old_nodes.isinstance(TextNode):
-        split_text = old_nodes.split(delimiter)
-        return_text = []
-        if len(split_text) < 3:
-            raise Exception("That is not correct Markdown formatting")
-        for i, node in enumerate(split_text):
-            if 2 % i == 0:
-                return_text.append([node, text_type])
-            else:
-                return_text.append([node, text_type_text])
-        return return_text
-    else:
-        return [old_nodes, text_type]
